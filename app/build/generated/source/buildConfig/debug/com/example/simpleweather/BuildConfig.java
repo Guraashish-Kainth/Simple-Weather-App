ac747d4b7a1fe854b6cc35b1b9d91219cefb45aa
Guraashish-Kainth <guraashish.kainth@gmail.com>
2022-02-21 18:48:39 -0500

    Simpler Weather App

---
+++ app/build/generated/source/buildConfig/debug/com/example/simpleweather/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.simpleweather;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.simpleweather";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String API_KEY = "a9f9914d1dc79d3e7cb7f9972fc49a0b";
+}
